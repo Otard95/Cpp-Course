1f1db2c2b747396558acbdc33790f74f1a36c312
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-03-19 16:26:43 +0100

    formated constructer

--- VS17/SDL_Components/Sprite_Renderer/CDP/Component.cpp
+++ VS17/SDL_Components/Sprite_Renderer/CDP/Component.cpp
@@ -3,7 +3,8 @@
 
 using namespace CDP;
 
-CDP::Component::Component(std::vector<std::shared_ptr<Component>> &components, GameObject & gobj)
+Component::Component(std::vector<std::shared_ptr<Component>> &components,
+													GameObject & gobj)
 	: m_sibling_components(components)
 	, m_game_object(gobj)
 {}
